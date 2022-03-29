6191ba9bf53b4c91784f17d3db7c1b21a440fffd
erissonbcs <erissonbcs@gmail.com>
2022-03-28 22:14:43 -0300

    Feign client

--- hr-payroll/src/main/java/com/mscourse/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mscourse/hrpayroll/services/PaymentService.java
@@ -1,31 +1,30 @@
 package com.mscourse.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.mscourse.hrpayroll.entities.Payment;
 import com.mscourse.hrpayroll.entities.Worker;
+import com.mscourse.hrpayroll.feignclients.WorkerFeignCliente;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignCliente workerFeignCliente;
 	
 	public Payment getPayment(long workerId, int days) {
+		Worker worker = workerFeignCliente.findById(workerId).getBody();
+		return new Payment(worker.getNome(), worker.getDailyIncome(), days);
+	}
+	
+	//RestTemplate
+	/*public Payment getPayment(long workerId, int days) {
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("id", ""+workerId);
 		
 		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 		return new Payment(worker.getNome(), worker.getDailyIncome(), days);
-	}
+	}*/
 
 }
